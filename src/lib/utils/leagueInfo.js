3e8bbb4f08d08ea747b016d7225f0ef61479b47a
littlegiantsbanes <89208910+littlegiantsbanes@users.noreply.github.com>
2021-08-19 11:12:18 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,8 +1,8 @@
 /*   STEP 1   */
-export const leagueID = "your_league_id"; // your league ID
-export const leagueName = "your_league_name"; // your league name
-export const dues = 100; // (optional) used in template constitution page
-export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
+export const leagueID = "718875521139699712"; // your league ID
+export const leagueName = "League of Brones"; // your league name
+export const dues = 200; // (optional) used in template constitution page
+export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
 
 /*   STEP 2   */
 export const homepageText = `
